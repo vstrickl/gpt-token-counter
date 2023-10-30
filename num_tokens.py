3daedceca5ed9613d87ecb28f9dadfbc01057a2a
Vonique Stricklen <vstrickl.git@gmail.com>
2023-10-30 13:44:27 -0700

    Created new script

--- num_tokens.py
+++ num_tokens.py
@@ -4,12 +4,12 @@ def num_tokens(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         text = f.read()
 
-    enc = tiktoken.encoding_for_model("gpt-4")
+    enc = tiktoken.encoding_for_model('gpt-4')
     tokens = list(enc.encode(text))
     token_count = len(tokens)
 
     return token_count
 
-file_path = './transcripts/14_Adisa/Adisa_(Full).txt'
+file_path = './promtps/highlight.txt'
 token_count = num_tokens(file_path)
 print(f'The file contains {token_count} tokens.')
